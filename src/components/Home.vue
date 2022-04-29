1fcafc0ad193567024aa652526dd688a70854317
eloygm92 <eloygm.92@uma.es>
2022-04-29 19:01:06 +0200

    Router Done

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script setup>
+  import { useRouter } from 'vue-router';
+
+  let token = undefined;
+
+
+  token = localStorage.getItem('jwt_token');
+
+  const router = useRouter();
+
+  if(token){
+    router.replace('/dashboard');
+  } else {
+    router.replace('/login');
+  }
+  
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/Login";
+import Dashboard from "@/components/Dashboard";
+import Home from "@/components/Home";
 
 const routes = [
-    { path: '/', name: 'Login', component: Login }
+    { path: '/', name: 'Home', component: Home},
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard}
 ];
 
 const router = createRouter({
